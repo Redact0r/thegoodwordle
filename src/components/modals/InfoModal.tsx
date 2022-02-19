3532b2e55a7c8fe2099fe879a9365321c415f53e
Tyler Sharp <johntylersharp@gmail.com>
2022-02-19 17:38:55 -0500

    first round edits

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,6 +10,12 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
+        The Good Wordle is a Bible-based spin off the game Wordle. Words can be
+        the names of people, places, things or any word in the Old or New
+        Testament. <br />
+        <br />
+      </p>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
         Guess the word in 6 tries. After each guess, the color of the tiles will
         change to show how close your guess was to the word.
       </p>
@@ -18,53 +24,53 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="W"
+          value="B"
           status="correct"
         />
+        <Cell value="I" />
+        <Cell value="B" />
+        <Cell value="L" />
         <Cell value="E" />
-        <Cell value="A" />
-        <Cell value="R" />
-        <Cell value="Y" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
+        The letter B is in the word and in the correct spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="P" />
-        <Cell value="I" />
+        <Cell value="F" />
+        <Cell value="L" />
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="L"
+          value="E"
           status="present"
         />
-        <Cell value="O" />
-        <Cell value="T" />
+        <Cell value="S" />
+        <Cell value="H" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
+        The letter E is in the word but in the wrong spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
-        <Cell value="A" />
         <Cell value="G" />
-        <Cell isRevealing={true} isCompleted={true} value="U" status="absent" />
+        <Cell value="R" />
+        <Cell value="A" />
+        <Cell isRevealing={true} isCompleted={true} value="P" status="absent" />
         <Cell value="E" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
+        The letter P is not in the word in any spot.
       </p>
 
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
-        This is an open source version of the word guessing game we all know and
-        love -{' '}
+        This is build on an open source version of the word guessing game we all
+        know and love -{' '}
         <a
           href="https://github.com/cwackerfuss/react-wordle"
           className="underline font-bold"
         >
-          check out the code here
+          check out the original code here (and make your own!)
         </a>{' '}
       </p>
     </BaseModal>
